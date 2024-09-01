8a725d1ed728696c47dc9259e1727495e6f3513d
“Phirun <tus27739@temple.edu>
2024-09-01 18:16:45 -0400

    Updated print_random.c

--- print_random.c
+++ print_random.c
@@ -1,9 +1,19 @@
-//Given Project code
-char* rand_string_alloc(size_t size)
+#include <stdio.h>
+#include <stdlib.h> // standard library for memory allocation and random number
+#include <time.h>	// time library to seed random number
+#include "random.h" // header file for randchar function declaration
+
+int main()
 {
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size);
-     }
-     return s;
+	int a;
+
+	/* seed the randomizer */
+	srand((unsigned)time(NULL));
+
+	printf("Today's random word: ");
+	for (a = 0; a < 7; a++)	 // loop generate and print 7 random words
+		putchar(randchar()); // call randchar
+	putchar('\n');			 // print newline after the word
+
+	return (0);
 }
