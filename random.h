1733ce9a4c6b0ea271e025cb5ea408fa3e949d4a
“Phirun <tus27739@temple.edu>
2024-09-01 18:18:30 -0400

    random.h

---
+++ random.h
@@ -0,0 +1,7 @@
+// Phirun Chhay Lab(Git)
+#ifndef RANDOM_H // prevent multiple inclusions
+#define RANDOM_H
+
+char randchar();    // declare ranchar function
+
+#endif  // end of preprocessor directive
